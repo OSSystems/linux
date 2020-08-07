e931510d060456481be1e3a00bbf9fd5444ffeca
Antony Abee Prakash XV <antonyabee.prakashxv@adlinktech.com>
2020-08-07 11:57:23 +0530

    Add instructive comments and clarify kernel message indicating the CPU variant
    - U-Boot overwrites the dummy size in the DDR3 memory node with the actual
      value, don't bother editing it.
    - Comment that wdog1 needs to be explicitly disabled and that watchdog uses
      WDOG2_B output: Wdog2 was meant as TrustZone watchdog and doesn't connect
      to the System Reset Controller (SRC), but we need an external reset path
      via -->BMC-->POR to reset module and carrier devices anyway, and
      pin-muxing only left WDOG2_B unoccupied. And so far we're not interested
      in TrustZone.
    - The kernel code mostly talks about i.MX6 "Quad" and "DualLite", but they
      actually mean "Quad/Dual" and "DualLite/Solo".
    
    Signed-off-by: Antony Abee Prakash XV <antonyabee.prakashxv@adlinktech.com>
    Signed-off-by: Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -346,7 +346,7 @@ static void __init imx6q_init_machine(void)
 	if (cpu_is_imx6q() && imx_get_soc_revision() >= IMX_CHIP_REVISION_2_0)
 		imx_print_silicon_rev("i.MX6QP", IMX_CHIP_REVISION_1_0);
 	else
-		imx_print_silicon_rev(cpu_is_imx6dl() ? "i.MX6DL" : "i.MX6Q",
+		imx_print_silicon_rev(cpu_is_imx6dl() ? "i.MX6DL/S" : "i.MX6Q/D",
 				imx_get_soc_revision());
 
 	parent = imx_soc_device_init();
